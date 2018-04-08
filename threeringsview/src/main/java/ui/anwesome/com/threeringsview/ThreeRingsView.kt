02fb726e0ccaa79b67a639cd025a63936b354ec2
Anwesh43 <anweshthecool0@gmail.com>
2018-04-09 02:38:39 +0530

    adding ThreeRingsView class

--- threeringsview/src/main/java/ui/anwesome/com/threeringsview/ThreeRingsView.kt
+++ threeringsview/src/main/java/ui/anwesome/com/threeringsview/ThreeRingsView.kt
@@ -3,3 +3,25 @@ package ui.anwesome.com.threeringsview
 /**
  * Created by anweshmishra on 09/04/18.
  */
+
+import android.content.*
+import android.view.*
+import android.graphics.*
+
+class ThreeRingsView (ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
